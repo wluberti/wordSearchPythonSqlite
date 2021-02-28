15be445f6b8a39e9facfbfb2e7f66f6a36629b02
Wouter Luberti <wouter.luberti@xs4all.net>
2021-02-28 19:30:22 +0100

    Initial commit

---
+++ database.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+class DatabaseManager:
+    '''
+        Database Manager
+        Special thanks to: https://cppsecrets.com/users/110711510497115104971101075756514864103109971051084699111109/Python-SQLite-Check-If-Database-Exists-or-Not.php
+    '''
+
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.conn = None
+
+    def check_database(self, log):
+        log.info(self.conn)
+        if self.conn == None:
+            try:
+                self.conn = sqlite3.connect(self.db_name, uri=True)
+                log.info(f'Succesfully connected to {self.db_name} database.')
+
+                if self.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='words';") != 'words':
+                    self.execute("CREATE TABLE IF NOT EXISTS words (word TEXT PRIMARY KEY)")
+                    log.info(f'Created "words" table because it did not exist.')
+
+            except sqlite3.OperationalError as err:
+                log.error(f'Error: {err}')
+
+            finally:
+                return False
+
+        log.info(self.count_words())
+        return True
+
+    def count_words(self):
+        return self.execute("SELECT COUNT(*) FROM words")[0][0]
+
+    def close_connection(self):
+        if self.conn is not None:
+            self.conn.close()
+
+    def execute(self, sql_string):
+        cursorObj = self.conn.cursor()
+        cursorObj.execute(sql_string)
+        self.conn.commit()
+
+        return cursorObj.fetchall()
---
+++ main.py
@@ -0,0 +1,92 @@
+# loosly based on
+# https://tutorial-academy.com/uwsgi-nginx-flask-python-sqlite-docker-example/
+
+from database import DatabaseManager
+import unidecode
+import json
+import os
+from flask import Flask, request, send_file, redirect, url_for, render_template
+
+app = Flask(__name__)
+app.db = DatabaseManager('words.db')
+# app.db = DatabaseManager(':memory:')
+app.dictionary = 'wordlist.txt'
+
+
+@app.route("/")
+def main(**args):
+    return render_template('index.html', args = args)
+
+@app.route('/favicon.ico')
+def favicon():
+    return redirect(url_for('static', filename='favicon.ico'))
+
+
+@app.route("/prepare", methods=["GET"])
+def prepare():
+    if app.db.check_database(app.logger) and app.db.count_words() < 100:
+        populate_database()
+        app.logger.info('Populated database.')
+        status = 'Populated database.'
+    else:
+        status = 'Database already prepared.'
+
+    return render_template('test.html', status = status)
+
+@app.route("/check", methods=["GET"])
+def check():
+    status = ''
+    try:
+        if app.db.check_database(app.logger):
+            numberOfWords = app.db.count_words()
+            status = f'Found {numberOfWords} records.'
+    except Exception as e:
+        app.logger.error(f'Error: {e}')
+
+    return render_template('test.html', status = status)
+
+@app.route("/close", methods=["GET"])
+def close():
+    status = []
+    try:
+        app.db.close_connection()
+        status.append('ok')
+    except Exception as e:
+        status.append(f'Error: {e}')
+
+    return render_template('test.html', status = status)
+
+def populate_database():
+    with open(app.dictionary) as wl:
+        wordset = set() # Use use to create a unique list
+        data = wl.readlines()
+        for word in data:
+            # Remove any special characters
+            word = unidecode.unidecode(word).strip()
+
+            # Skip all words that are not allowed in Scrabble
+            if ' ' in word: continue
+            if '-' in word: continue
+            if '\'' in word: continue
+            if any(l.isupper() for l in word): continue
+            if any(l.isdigit() for l in word): continue
+
+            # Add word to set
+            wordset.add(word)
+
+    words = "'), ('".join(wordset)
+    sql = f"INSERT OR IGNORE INTO words VALUES ('{words}')"
+
+    status = []
+    try:
+        status.append(app.db.execute(sql))
+        # status = 'ok'
+    except Exception as e:
+        status.append(f'Error: {e}')
+
+    return status
+
+
+if __name__ == "__main__":
+    # Port 80 configuration to run via docker-compose up
+    app.run(host="0.0.0.0", port=80, debug=True)
